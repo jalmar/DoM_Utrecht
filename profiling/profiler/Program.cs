4a7a091d6eb746039803e93c913f6941e8f0b77f
Jens den Braber <jens.denbraber@gmail.com>
2014-10-08 12:10:44 +0200

    Read multi page tiff images
    Read multi page tiff images in c++ or c#

---
+++ profiling/profiler/Program.cs
@@ -0,0 +1,37 @@
+﻿using BitMiracle.LibTiff.Classic;
+
+
+namespace profiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Tiff image = Tiff.Open(@"E:\storm_data\test.tif", "r"))
+            {
+                if (image == null)
+                    return;
+
+                bool subDirectory = image.SetSubDirectory(0);
+                bool directory = image.SetDirectory(0);
+                bool isTiled = image.IsTiled();
+                string fileName = image.FileName();
+                bool lastDirectory = image.LastDirectory();
+
+                short numberOfDirectories = image.NumberOfDirectories();
+                int numberOfStrips = image.NumberOfStrips();
+                int numberOfTiles = image.NumberOfTiles();
+
+                System.Console.Out.WriteLine("{0} directories in {1} using the NumberOfDirectories() method", numberOfDirectories, image.FileName());
+
+                int dircount = 0;
+                do
+                {
+                    dircount++;
+                } while (image.ReadDirectory());
+
+                System.Console.Out.WriteLine("{0} directories in {1} using iterator", dircount, image.FileName());
+            }
+        }
+    }
+}
--- profiling/profiling/profiling.cpp
+++ profiling/profiling/profiling.cpp
@@ -2,13 +2,17 @@
 //
 
 #include "stdafx.h"
-
 #include <iostream>
+
+#include "tiffreader.h"
+#include <tiffio.h>
 
 #include <CL/cl.h>
 //#include <CL/cl_gl.h>
 //#include <CL/cl_platform.h>
 //#include <CL/clext.h>
+
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -17,9 +21,10 @@ int _tmain(int argc, _TCHAR* argv[])
 	cl_uint ok = 1;
 	clGetPlatformIDs(ok, &test, &num);
 
-	std::cout << "test" << std::endl;
-	std::cout << "Number of OpenCL platforms found: " << num << std::endl;
+	cout << "test" << std::endl;
+	cout << "Number of OpenCL platforms found: " << num << std::endl;
 
+	Load_image();
+	
 	return 0;
 }
-
