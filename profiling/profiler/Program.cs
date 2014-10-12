eeb6bb5481b9d70c6a1e3046bfa709748b1fa678
Jens den Braber <jens.denbraber@gmail.com>
2014-10-12 22:14:45 +0200

    add missing files
    add missing files

--- profiling/profiler/Program.cs
+++ profiling/profiler/Program.cs
@@ -1,51 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using BitMiracle.LibTiff.Classic;
-using profiler.io;
-using CsvReader = profiler.io.CsvReader;
-
+using System.Windows.Forms;
 
 namespace profiler
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-           var reader = new CsvReader();
-           List<DataRecord> readCsvList = reader.ReadCsvList(@"E:\spots_snr_3_SD_2.2_coords _test.csv");
-
-            foreach (var dataRecord in readCsvList)
-            {
-                Console.WriteLine(String.Format("dataRecord.id = {0}   dataRecord.X = {1}   dataRecord.Y = {2}   dataRecord.Intensity = {3} \n", dataRecord.Id, dataRecord.X, dataRecord.Y, dataRecord.Intensity));
-            }
-            
-
-
-            using (Tiff image = Tiff.Open(@"E:\myMultipageFile.tif", "r"))
-            {
-                if (image == null)
-                    return;
-
-                bool subDirectory = image.SetSubDirectory(0);
-                bool directory = image.SetDirectory(0);
-                bool isTiled = image.IsTiled();
-                string fileName = image.FileName();
-                bool lastDirectory = image.LastDirectory();
-
-                short numberOfDirectories = image.NumberOfDirectories();
-                int numberOfStrips = image.NumberOfStrips();
-                int numberOfTiles = image.NumberOfTiles();
-
-                System.Console.Out.WriteLine("{0} directories in {1} using the NumberOfDirectories() method", numberOfDirectories, image.FileName());
-
-                int dircount = 0;
-                do
-                {
-                    dircount++;
-                } while (image.ReadDirectory());
-
-                System.Console.Out.WriteLine("{0} directories in {1} using iterator", dircount, image.FileName());
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
